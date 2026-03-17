e449ed8e9b7728cb7d8e906b2c3c44e7fa0e8453
Towering474 <tiagusxavier@gmail.com>
2026-03-17 01:41:34 +0000

    16/3 adjustments

--- Cache/TypeScript/Src/Assets/onButtonChange.js
+++ Cache/TypeScript/Src/Assets/onButtonChange.js
@@ -1,6 +1,7 @@
 //@input Component.ScriptComponent buttonScript  // drag the button script here in Inspector
 //@input Component.ScriptComponent imageController
 //@input Component.ScriptComponent objController
+//@input Component.ScriptComponent buttonImageController
 
 // Toggle state (tracks ON/OFF)
 var isToggled = false;
@@ -9,19 +10,23 @@ var tId = 0;
 
 function onbuttonValueChanged() {
     print("button value changed:" + isToggled);
-    
+
     // WIP check button state and call script.imageController.updateVisibility(toggle)
     isToggled = !isToggled;
     textureId = textureId + 1;
     tId = textureId % 3;
 
     print("button value changed:" + textureId + tId);
+
     // Rotate between textures
     if (script.imageController && script.imageController.updateTexture) {
         script.imageController.updateTexture(tId);
     }
-    
 
+    
+    if (script.buttonImageController && script.buttonImageController.updateTexture) {
+        script.buttonImageController.updateTexture(tId);
+    }
     /*
     // Call visibility update in image controller, if exists
     if (script.imageController && script.imageController.updateVisibility) {
@@ -29,7 +34,6 @@ function onbuttonValueChanged() {
     }
 
     // Optional: call object controller toggle function if needed
-    
     if (script.objController && script.objController.updateVisibility) {
         script.objController.updateVisibility(!isToggled);
     }
@@ -38,22 +42,22 @@ function onbuttonValueChanged() {
 
 // Wait for Lens to start before attaching listeners
 script.createEvent("OnStartEvent").bind(function() {
-    if (!script.buttonScript) {     
+    if (!script.buttonScript) {
         print("button script not assigned!");
         return;
     }
-    
+
     // Subscribe to the event
     if (script.buttonScript.onPressDown) {
         script.buttonScript.onPressDown.add(onbuttonValueChanged);
     } else {
-        print(" buttonScript has no onValueChanged event!");
+        print("buttonScript has no onValueChanged event!");
     }
 
     print("Listening to button value changes!");
 
     // WIP add init for button toggle variable
-    
+
     /*
     // Initialize toggle state
     isToggled = true;
